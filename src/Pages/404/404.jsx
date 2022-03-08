6de36a7e9d85b2f2f71ef4285f8b4f08c64895b2
jrmygp <fat.snorlax17@gmail.com>
2022-03-08 10:17:20 +0700

    ui: added 404 on page not found

--- src/Pages/404/404.jsx
+++ src/Pages/404/404.jsx
@@ -1,7 +1,7 @@
 import { Text } from "@chakra-ui/react"
 const NotFoundPage = () => {
     return (
-        <Text as="h1">Page Not Found</Text>
+        <Text as="h1">404 Page Not Found</Text>
     )
 }
 
